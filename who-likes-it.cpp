63e4396574c73400e9c778331255764960bdd907
Thomas Dougherty <105173634+d0ugherty@users.noreply.github.com>
2023-07-14 09:23:07 -0400

    Refactor who-likes-it.cpp

--- who-likes-it.cpp
+++ who-likes-it.cpp
@@ -6,7 +6,6 @@ std::string likes(const std::vector<std::string> &names);
 
 int main() {
     std::vector<std::string> names = {"Alex", "Jacob", "Mark", "Max"};
-
     std::cout << likes(names);
     return 0;
 }
@@ -14,23 +13,17 @@ int main() {
 
 
 std::string likes(const std::vector<std::string> &names) {
-    std::string likeString;
     int length = names.size();
     switch(length) {
         case 0:
-            likeString = "no one likes this";
-            return likeString;
+            return "no one likes this";
         case 1:
-            likeString = names[0] + " likes this";
-            return likeString;
+            return names[0] + " likes this";
         case 2:
-            likeString = names[0] + " and " + names[1] + " like this";
-            return likeString;
+            return names[0] + " and " + names[1] + " like this";
         case 3:
-            likeString = names[0] + ", " + names[1] + " and " + names[2] + " like this";
-            return likeString;
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
         default:
-            likeString = names[0] + ", " + names[1] + " and " + std::to_string(length - 2) + " others like this";
-            return likeString;
+            return names[0] + ", " + names[1] + " and " + std::to_string(length - 2) + " others like this";
         }
 }
