574ffd163ca8e6b3cb0b4b03fecf4b6af6f87967
thomas <tdougherty16@gmail.com>
2023-07-25 21:02:08 -0400

    encryption: alternating split

--- cpp/alt-split.cpp
+++ cpp/alt-split.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 
@@ -6,15 +7,10 @@
 std::string encrypt(std::string, int n);
 std::string decrypt(std::string, int n);
 
-
-int main() {
-    std::string test = "012345";
-    std::cout << "Encrypt:  " << test << std::endl;
-    std::cout << "Result: " << encrypt(test, 2) << std::endl;
-    std::cout << "Decrypt: " << "hsi  etTi sats!" << std::endl; 
-    std::cout << "Result: " << decrypt("hsi  etTi sats!", 1) << std::endl;
+int main () {
+    encrypt("This is a test!", 0);
+    std::cout << decrypt("hsi  etTi sats!", 1) << std::endl;
     return 0;
-
 }
 
 std::string encrypt(std::string text, int n) {
@@ -32,35 +28,35 @@ std::string encrypt(std::string text, int n) {
     return text;
 }
 
-
 /** Inverse of the encryption function
  *  -Now we start with non-empty string objects for odd & even as opposed to
  *  to the empty ones in the encrypt function
  *  
  *  **/
 std::string decrypt(std::string encryptedText, int n) {
-    if (encryptedText.empty() || n < 1) return encryptedText;
-    
-    int textLength = encryptedText.length();
-    int textHalf = textLength / 2;
-    while (n > 0) {
-        std::string odd = encryptedText.substr(0, textHalf);
-        std::string even = encryptedText.substr(textHalf);
-        //encryptedText = "";
-        int evenLength = even.length();
-
-        for(int i = 0; i <= textHalf; i++) {
-            if(i % 2 == 0) {
-                encryptedText[i] += even[i];
-                std::cout << "EVEN " << even[i] << std::endl;
-            } else {
-                encryptedText[i] += odd[i];
-                std::cout << "ODD" << odd[i] << std::endl;
-            }
-            //(i % 2 == 0) ? encryptedText += even[i] : encryptedText += odd[i];
-        }
-        n--;
+    if (encryptedText.empty() || n < 1) {
+        return encryptedText;
     }
-    return encryptedText;
-}
 
+    int textLength, textHalf;
+    std::string odd, even;
+
+    while (n > 0) {
+        textLength = encryptedText.length();
+        textHalf = textLength / 2;
+        odd = encryptedText.substr(0, textHalf);
+        even = encryptedText.substr(textHalf);
+        encryptedText = "";
+
+        for (int i = 0; i < textHalf; i++) {
+            encryptedText += even[i];
+            encryptedText += odd[i];
+        }
+      
+        if (textLength % 2 != 0) {
+            encryptedText += even[textHalf];
+        }
+      n--;
+    }
+ return encryptedText;
+}
