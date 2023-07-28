a52753856841825573e5b011c411f9e9cbb2c060
thomas <tdougherty16@gmail.com>
2023-07-27 21:55:39 -0400

    split strings

---
+++ cpp/split-string.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> solution(const std:: string &s);
+
+
+int main() {
+    std::cout << "lol" << std::endl;
+    solution("InteriorCrocodileAlligato");
+    solution("InteriorCrocodileAlligator");
+    return 0;
+}
+std::vector<std::string> solution(const std::string &s)
+{
+    std::vector<std::string> result;
+    std::string temp = "";
+    for(int i = 0; i < s.length(); i += 2){
+        temp = s.substr(i,2);
+        temp.length() == 2 ? result.push_back(temp) : result.push_back(temp += '_');    
+    }
+    return result; // Your code here
+}
