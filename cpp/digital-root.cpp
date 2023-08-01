d33a887e6c5bedf7efecbc8dfee5a88994709c66
thomas <tdougherty16@gmail.com>
2023-07-31 22:35:36 -0400

    digital root COMPLETE

--- cpp/digital-root.cpp
+++ cpp/digital-root.cpp
@@ -8,19 +8,16 @@ int main() {
     std::cout << "test int: " << std::to_string(test) << std::endl;
     std::cout << "digital root: " << std::to_string(digital_root(test)) << std::endl;
     return 0;
+
 }
-
-int digital_root(int n){
-    //base case
-    std::cout << "n = " << n <<std::endl;
-    std::cout << "ceil " << ceil(log10(n)) << std::endl;
-    if(ceil(log10(n)) == 1) {
-        return 0;
+int digital_root(int n) { 
+    while(n >= 10) {
+        int sum = 0;
+        while(n != 0 ) { 
+            sum += n % 10;
+            n = n / 10;
+        }
+        n = sum;
     }
-    if(n == 0){
-        return 0;
-    }
-
-    return n % 10 + digital_root(n / 10);    
-    
+    return n;
 }
