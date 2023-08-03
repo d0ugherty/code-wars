232ff485c79b924c81cfabcc7feb7536c73fdeb0
thomas <tdougherty16@gmail.com>
2023-08-02 23:54:21 -0400

    bestplanet.py incomplete

--- python/bestplanet.py
+++ python/bestplanet.py
@@ -1,30 +1,32 @@
+import re
+
 REQUIRED_ELEMENTS = 'H', 'C', 'N', 'O', 'P', 'Ca'
 
 def best_planet(solar_system, max_size):
-    elements = ''.join(str(element) for element in REQUIRED_ELEMENTS)
-    print(elements)
-    suitable_planets = get_suitable_planets(solar_system, elements)
-    best_planet = get_best_size(suitable_planets, max_size)
-    return best_planet
+    print("max size: " + str(max_size))
+    return get_best_size(get_suitable_planets(solar_system), max_size)
 
-def get_suitable_planets(solar_system, elements):
+def get_suitable_planets(solar_system):
+    elements = r'(?=.*H)(?=.*C)(?=.*N)(?=.*O)(?=.*P)(?=.*Ca)?[A-Za-z0-9_]*$'
     suitable_planets = []
     for planet in solar_system:
-        # replace this with a regex
-        if elements in planet:
+        if re.search(elements, planet):
             suitable_planets.append(planet)
     return suitable_planets
 
 def get_best_size(suitable_planets, max_size):
-    best_planet = suitable_planets[0]
-    best_size = int(best_planet.split('_')[1])
-    for planet in suitable_planets:
-        size = int(planet.split('_')[1])
-        if abs(max_size - size) < abs(max_size - size):
-            best_planet = planet
-    return best_planet
-        
-
-solar_system = ['OHNCCaP_100', 'OHC_200', 'OCa_50', 'OHCCaP_400', 'OHNCCaP_225']
-best_planet(solar_system, 225)
-
+    if suitable_planets:
+        best_planet = suitable_planets[0]
+        best_size = int(best_planet.split('_')[1])
+        for planet in suitable_planets:
+            print("planet: " )
+            curr_size = int(planet.split('_')[1])
+            print("current best planet: " + best_planet)
+            if abs(max_size - curr_size) <= abs(best_size - curr_size):
+                best_planet = planet
+        if best_size > max_size:
+            return ''
+        else:
+            return best_planet
+    else:
+        return ''
