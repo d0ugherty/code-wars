0c649e6470f90596618e6cad85eae73fa08a5821
Thomas Dougherty <tdougherty16@gmail.com>
2023-09-15 11:05:52 -0400

    refactor narc.cpp

--- cpp/narc.cpp
+++ cpp/narc.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 
-bool narcissistic(int value);
-
-int main () {
-    int value = 371;
-    std::cout << value << "\n"; 
-    std::cout << narcissistic(value) << "\n";
-    return 0;
-}
-
 bool narcissistic( int value ){
   //Code away
   int result = 0;
@@ -20,7 +11,6 @@ bool narcissistic( int value ){
   while(valDivided != 0) {
     valDivided = valDivided / 10;
     digitCount++;
-    std::cout << "digit count = " << digitCount << std::endl;
   }
   
   valDivided = value;
@@ -29,20 +19,24 @@ bool narcissistic( int value ){
     //first digit
     if(i == 0){
         currentDigit = value % 10;
-        std::cout << "current digit = " << currentDigit << std::endl;
       } else {
         valDivided = valDivided / 10;
         currentDigit = valDivided % 10;
-        std::cout << "current digit = " << currentDigit << std::endl; 
       }
     // and raise them to the power of digit count
     int raised = 1;
     for(int i = 0; i < digitCount; i++) {
         raised *= currentDigit;
-        std::cout << "raised = " << raised << std::endl;
    }
     result += raised;
-    std::cout << "result = " << result << std::endl;
   }
 return result == value;
 }
+
+int main () {
+    int value = 371;
+    std::cout << value << "\n"; 
+    std::cout << narcissistic(value) << "\n";
+    return 0;
+}
+
