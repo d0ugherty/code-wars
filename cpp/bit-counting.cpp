cd9ee0b8e74a4b343395dbb71e399c1124526aaa
Thomas Dougherty <tdougherty16@gmail.com>
2023-09-15 11:13:35 -0400

    refactor bit-counting.cpp

--- cpp/bit-counting.cpp
+++ cpp/bit-counting.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-unsigned int countBits(unsigned long long n);
-
-int main (){
-    std::cout<<countBits(12345)<<std::endl;
-    return 0;
-}
-
-
 unsigned int countBits(unsigned long long n) {
     int bitCount = 0;
-    while(n !=0) {
-        if(n % 2 == 1){
+    while (n !=0) {
+        if (n % 2 == 1) {
             bitCount++;
         }
         n /= 2;
     }
     return bitCount;
+}
+
+/** Main function not part of solution
+ **/
+int main (){
+    std::cout << countBits(12345) << std::endl;
+    return 0;
 }
