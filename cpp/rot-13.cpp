876d61d6cfc1ec91df056ff0368015393140b0ab
thomas <tdougherty16@gmail.com>
2023-09-15 19:39:49 -0400

    working on rot 13

--- cpp/rot-13.cpp
+++ cpp/rot-13.cpp
@@ -2,13 +2,7 @@
 #include <iostream>
 #include <regex>
 
-std::string rot13(const std::string& str);
-
-int main() {
-    std::cout << rot13("EBG13 rknzcyr.") << std::endl;
-}
-
-std::string rot13(const std::string& str) {
+std::string rot13(const std::string &msg) {
     std::string result = "";
     std::regex pattern("[$&+,:;=?@#|0-9'<>.^*()%!-]");
     std::map<char,int> map1;
@@ -17,23 +11,35 @@ std::string rot13(const std::string& str) {
     //map for cipher
     for(int i = 1; i < 27; i++){
         map1[char(64 + i)] = i;
+        //std::cout << map1[char(64+1)];
     }
 
     map2[0] = 'Z';
 
     for(int i = 1; i < 26; i++){
         map2[i] = char(64+i);
+        std::cout << map2[i];
     }
 
-    for(int i = 0; i < str.size(); i++){
-        if(str[i] != ' '){
-            if(std::regex_match(std::to_string(str[i]), pattern)){
-                int number = ((map1[str[i]] + 13) % 26); 
+    for(int i = 0; i < msg.size(); i++){
+        if(msg[i] != ' '){
+            //if(std::regex_match(std::to_string(msg[i]), pattern)){
+                int number = ((map1[msg[i]] + 13) % 26);
+                std::cout << "map1 mesg[i] + 13 = " << map1[msg[i]] + 13 << std::endl;
+                std::cout<<(map1[msg[i]] + 13) % 26 << std::endl;
                 result += map2[number];
-            }
+           // }
         } else {
             result += " ";
         }
     }
     return result;
 }
+
+int main(int argc, char* argv[]) {
+    std::string test = argv[1];
+    std::cout << "ciphering " << test << std::endl;
+    std::cout << rot13(test) << std::endl;
+}
+
+
