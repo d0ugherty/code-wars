a51bed9b8a84976c4d36bfe4da7c3efe128de44a
Thomas Dougherty <tdougherty16@gmail.com>
2023-11-03 13:39:04 -0400

    Update twosum.cpp

--- cpp/twosum.cpp
+++ cpp/twosum.cpp
@@ -1,16 +1,6 @@
 #include <vector>
 #include <iostream>
 
-std::pair<std::size_t, std::size_t> two_sum(const std::vector<int>& numbers, int target);
-
-int main() {
-    std::cout << "TEST VALUES: 1234, 5678, 9012     TARGET: 14690" << std::endl;
-    std::pair<std::size_t, std::size_t> result = two_sum({1234, 5678, 9012}, 14690);
-    std::cout << "Result: " << result.first << " " << result.second << std::endl;
-    return 0;
-}
-
-
 std::pair<std::size_t, std::size_t> two_sum(const std::vector<int>& numbers, int target) {
     
     std::size_t size = numbers.size();
@@ -23,4 +13,11 @@ std::pair<std::size_t, std::size_t> two_sum(const std::vector<int>& numbers, int
         }
     }
     return {0,0};
+}
+
+int main() {
+    std::cout << "TEST VALUES: 1234, 5678, 9012     TARGET: 14690" << std::endl;
+    std::pair<std::size_t, std::size_t> result = two_sum({1234, 5678, 9012}, 14690);
+    std::cout << "Result: " << result.first << " " << result.second << std::endl;
+    return 0;
 }
