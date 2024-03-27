209c88568ccea779ede6c05cba4eb2366447a95e
d0ugherty <tdougherty16@gmail.com>
2024-03-27 19:17:48 -0400

    count duplicates

---
+++ csharp/CountDuplicates/CountDuplicates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit;
+
+
+namespace CountDuplicates
+{
+	public class Kata
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(args);
+			string input = args.Length > 0 ? args[0] : "";
+			Console.WriteLine($"INPUT: {input}");
+			Console.WriteLine($"Characters with duplicates: {CountDupes(input)}");
+		}
+
+		public static int CountDupes(string input)
+		{
+			Dictionary<char, bool> dupes = new Dictionary<char, bool>();
+
+			char[] inputArray = input.ToLower().ToCharArray();
+
+			foreach (char ch in inputArray)
+			{
+				if (!dupes.TryAdd(ch, false))
+				{
+					dupes[ch] = true;
+				}
+			}
+
+			int count = 0;
+
+			foreach (KeyValuePair<char,bool> item in dupes)
+			{
+				if (item.Value)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+}
+
